5746ec84fbaa461e106ceff34d44f8aa938f0096
ZeLet1234 <zeletyt@gmail.com>
2025-01-14 02:13:20 +0100

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Słucha Spotify", "RyzerStock", "zapraszaj innych"];
+const statusMessages = ["🎧 Slucha Hender", "🎮 Gra w Hender", "Author: ZeLet"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
